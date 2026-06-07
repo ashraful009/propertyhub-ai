bdf3c53ab1c45a1957d69c3f838df7158514132d
Md_AsHrAFuL_IsLaM <121592381+ashraful009@users.noreply.github.com>
2026-06-07 16:45:52 +0600

    cloudinary setup

--- backend/database.sql
+++ backend/database.sql
@@ -13,3 +13,24 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+-- property manage
+CREATE TYPE property_status AS ENUM ('AVAILABLE', 'BOOKED', 'SOLD');
+
+-- Properties tabke
+CREATE TABLE properties (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(15, 2) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    address TEXT,
+    property_type VARCHAR(100),
+    bedrooms INT,
+    bathrooms INT,
+    area NUMERIC(10, 2), -- সsqure fit
+    images TEXT[], -- from Cloudinary image URL 
+    status property_status DEFAULT 'AVAILABLE',
+    vendor_id UUID REFERENCES users(id) ON DELETE CASCADE, -- vendor id
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ backend/src/config/cloudinary.ts
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary';
+import {CloudinaryStorage} from 'multer-storage-cloudinary';
+import dotenv from 'dotenv';
+import multer from 'multer';
+
+dotenv.config();
+
+// Cloudinary configuration
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+// multer storage setup
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary, 
+    params: async (req, file ) =>{
+        return{
+            folder: 'propertyhub_properties',
+            allowed_formats: ['jpg', 'jpeg', 'png', 'webp']
+        }
+    }
+})
+
+export const upload = multer({storage: storage})
