bfd67a95704e3f7710ff7a9097c9322e4e2e0bbd
Md_AsHrAFuL_IsLaM <121592381+ashraful009@users.noreply.github.com>
2026-06-08 09:52:36 +0600

    Frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-green-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind v4 is Working! 🚀
+      </h1>
+    </div>
+  )
+}
